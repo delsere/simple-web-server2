9187d6f58182863055cf0a6e2e9bcc1262352a43
delsere <fabio.delsere@gmail.com>
2017-01-02 22:17:59 +0100

    creata cartella template

--- main.go
+++ main.go
@@ -9,16 +9,22 @@ import (
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm() //Parse url parameters passed, then parse the response packet for the POST body (request body)
+	//Parse url parameters passed, then parse the response packet for the POST body (request body)
 	// attention: If you do not call ParseForm method, the following data can not be obtained form
-	fmt.Println(r.Form) // print information on server side.
+	r.ParseForm()
+
+	// print information on server side.
+	fmt.Println(r.Form)
+
 	fmt.Println("path", r.URL.Path)
 	fmt.Println("scheme", r.URL.Scheme)
 	fmt.Println(r.Form["url_long"])
+
 	for k, v := range r.Form {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 	}
+
 	fmt.Fprintf(w, "Hello astaxie!") // write data to response
 }
 
@@ -26,7 +32,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method) //get request method
 
 	if r.Method == "GET" {
-		t, err := template.ParseFiles("login.gtpl")
+		t, err := template.ParseFiles("template/login.gtpl")
 		if err != nil {
 			log.Fatal("Parsing login.gtpl failed: ", err)
 		}
